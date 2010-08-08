ea02e7ab0ea0fb5ded8bff346dee287648ec9c73
carloncho.lescano <carloncho.lescano@4bd8d8d1-940b-83e7-6b5e-19f916b8bc61>
2010-08-08 22:00:14 +0000

    PRUEBA

---
+++ trunk/prueba/src/pe/edu/upc/dew/tdd/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.tdd;
+
+/**
+ *
+ * @author u913970
+ */
+class Calculadora {
+
+    public Calculadora() {
+    }
+
+   public int sumar(int a, int b) {
+       return a + b;
+
+       
+    }
+
+}
